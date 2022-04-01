3926158d101244309ca5e075bbc91afca782cbab
Shamita V <96277671+Shamita07@users.noreply.github.com>
2022-04-01 04:41:04 +0000

    practice

--- p2final.c
+++ p2final.c
@@ -1,62 +1,38 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int input_side()
 {
   int a;
-  printf("enter the side of the triangle : \n");
+  printf("enter the side:\n");
   scanf("%d",&a);
   return a;
 }
-int check_scalene(int a, int b, int c)
+
+int check_scalene(int a,int b,int c)
 {
-  int tri;
-  if (a==b && b==c)
-  {
-  tri=0;
-  }
-  else if(a!=b && a==c) 
-  {
-    tri=1;
-  }
-  else if(a==b && a!=c)
-  {
-    tri=1;
-  }
-  else if(b==c && a!=b)
-  {
-    tri=1;
-  }
-  else 
-  {
-    tri=5;
-  }
-  return tri;
+  int s;
+  if(a!=b && b!=c && c!=a)
+  s=1;
+  else
+  s=0;
+  return s;
 }
-void output(int a, int b, int c, int tri)
+
+void output(int a, int b, int c, int s)
 {
-  
-  if(tri==0)
-  {
-  printf(" is equilateral ");
-  }
-  if(tri==1)
-  {
-  printf(" is isosceles ");
-  }
-  else if(tri==5)
-  {
-    printf("is scalene");
-  }
+  if (s=1)
+  printf("triangle with sides %d,%d,%d is scalene",a,b,c);
+  else
+  printf("triangle is not scalene");
 }
 
 int main()
 {
-  int m,n,o,y;
-  m=input_side();
-  n=input_side();
-  o=input_side();
-  y=check_scalene(m,n,o);
-  output(m,n,o,y);
+  int a,b,c,scalene;
+  a=input_side();
+  b=input_side();
+  c=input_side();
+  scalene=check_scalene(a,b,c);
+  output(a,b,c,scalene);
   return 0;
-
 }
--- p5final.c
+++ p5final.c
@@ -1,43 +1,37 @@
-
 #include <stdio.h>
 int input()
 {
   int a;
   printf("Enter the number\n");
-  scanf("%d%d",&a);
+  scanf("%d",&a);
   return a;
 }
 
-int find_gcd(int a, int b)
+int gcd(int a,int b)
 {
-  int gcd,t;
-  if( a >b)
-  {
-    t = a;
-    a = b;
-    b = t;
-  }
-  if ( b%a == 0)
-    return a;
-  for(int i=2; i<a/2; i++)
-  {
-    if ( a%i==0 && b%i==0)
-      gcd = i;
-  }
-  return gcd;
+  int r,i;
+  for(i=1;i<=a && i<=b;i++) 
+    {
+      if(a%i==0 && b%i==0)
+      {
+        r=i;
+      }
+    }
+  return r;
 }
 
-void output(int a, int b, int gcd)
-{
-  printf("The gcd %d and %d is %d\n",a,b,gcd);
+int output(int a, int b,int gcd)
+{ 
+  printf("the hcf is %d: \n",gcd);
 }
 
 int main()
 {
-  int a, b, gcd;
-  a = input();
-  b = input();
-  gcd = find_gcd(a,b);
-  output(a,b,gcd);
+  int a,b,r;
+  a=input();
+  b=input();
+  r=gcd(a,b);
+  output(a,b,r);
   return 0;
 }
+
